06e7f9a9bf85ccf5abdda61bf80c5a66bfd7a3be
ystaros <145158060+ystaros@users.noreply.github.com>
2024-01-21 14:19:12 -0500

    Framework created (#2)

---
+++ src/test/java/utils/TestData.java
@@ -0,0 +1,16 @@
+package utils;
+
+public final class TestData {
+    public static final String BASE_URL = "http://localhost:3000";
+
+    public static final String HOME_END_POINT = "/";
+    public static final String WOMEN_END_POINT = "/women";
+    public static final String MEN_END_POINT = "/men";
+    public static final String ACCESSORIES_END_POINT = "/accessories";
+    public static final String ABOUT_END_POINT = "/#footer-about";
+
+    public static final String WOMEN = "women";
+    public static final String MEN = "men";
+    public static final String ACCESSORIES = "accessories";
+    public static final String ABOUT = "About";
+}
