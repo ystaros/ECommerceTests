250fac024780918c44211356fb5a40d8e21735e0
ystaros <145158060+ystaros@users.noreply.github.com>
2024-04-07 13:22:01 -0400

    add DataProvider (#6)

--- src/test/java/tests/NavigationTest.java
+++ src/test/java/tests/NavigationTest.java
@@ -4,6 +4,7 @@ import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import utils.LoggerUtils;
 import utils.TestData;
@@ -13,11 +14,107 @@ import static utils.TestData.*;
 
 
 public final class NavigationTest extends BaseTest {
-    @Test (testName = "Test - landing on the Home Page")
+    @Test(testName = "Test - landing on the Home Page")
     public void TestBaseUrlLanding() {
         getPage().navigate(BASE_URL);
 
         assertThat(getPage()).hasURL(BASE_URL + HOME_END_POINT);
+    }
+
+    // @DataProvider - двумерный массив [][]
+    // @Para - двумерный массив [][]
+
+    @DataProvider(name = "menuNavigationTestData")
+    private Object[][] menuNavigationTestData() {
+//        Object[]][] testData = new Object[][]{{"1","11"}, {"2", "22"}, {"3", "33"}};
+
+//        return testData;
+//        return new Object[][]{{"1","11"}, {"2", "22"}, {"3", "33"}};
+        return new Object[][]{
+                {MEN, BASE_URL + MEN_END_POINT},
+                {WOMEN, BASE_URL + WOMEN_END_POINT},
+                {ACCESSORIES, BASE_URL + ACCESSORIES_END_POINT},
+                {ABOUT, BASE_URL + ABOUT_END_POINT}
+        };
+    }
+
+//    @Test(dataProvider = "menuNavigationTestData")
+//    public void testMenuNavigatesToCorrespondingPage(String menuName, String correspondingPageEndpoint) {
+//        if (getIsOnHomePage()) {
+//
+//            getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(menuName).setExact(true)).click();
+//
+//            assertThat(getPage()).hasURL(correspondingPageEndpoint);
+//        } else {
+//            Assert.fail();
+//        }
+//    }
+
+    @Test(dataProvider = "menuNavigationTestData")
+    public void testMenuNavigatesToCorrespondingPage(String menuName, String correspondingPageEndpoint) {
+        if (getIsOnHomePage()) {
+
+            getPage().locator("//nav[@class='navbar']//li/a[text()='" + menuName + "']").click();
+
+            assertThat(getPage()).hasURL(correspondingPageEndpoint);
+        } else {
+            Assert.fail();
+        }
+    }
+
+    @DataProvider(name = "bannerNavigationTestData")
+    private Object[][] getBannerNavigationTestData() {
+
+        return new Object[][]{
+                {0, BASE_URL + WOMEN_END_POINT},
+                {1, BASE_URL + MEN_END_POINT},
+                {2, BASE_URL + ACCESSORIES_END_POINT},
+        };
+    }
+
+    @Test(dataProvider = "bannerNavigationTestData")
+    public void testBannerNavigatesToCorrespondingPage(int bannerNumber, String correspondingPageEndpoint) {
+        if (getIsOnHomePage()) {
+
+            assertThat(getPage().locator("//section[@class='collection-container']/a/p")).hasCount(3);
+
+            getPage().locator("//section[@class='collection-container']/a/p").nth(bannerNumber).click();
+
+                assertThat(getPage()).hasURL(correspondingPageEndpoint);
+        } else {
+            Assert.fail();
+        }
+    }
+
+//    @DataProvider(name = "navigationTestData")
+//    private Object[][] getNavigationTestData() {
+//
+//        return new Object[][]{
+//                {"//nav[@class='navbar']//li/a[text()='" + MEN + "']", -1, BASE_URL + MEN_END_POINT},
+//                {"//nav[@class='navbar']//li/a[text()='" + WOMEN + "']", -1, BASE_URL + WOMEN_END_POINT},
+//                {"//nav[@class='navbar']//li/a[text()='" + ACCESSORIES + "']", -1, BASE_URL + ACCESSORIES_END_POINT},
+//                {"//nav[@class='navbar']//li/a[text()='" + ABOUT + "']", -1, BASE_URL + ABOUT_END_POINT},
+//                {"//section[@class='collection-container']/a/p", 0, BASE_URL + WOMEN_END_POINT},
+//                {"//section[@class='collection-container']/a/p", 1, BASE_URL + MEN_END_POINT},
+//                {"//section[@class='collection-container']/a/p", 2, BASE_URL + ACCESSORIES_END_POINT},
+//        };
+//    }
+
+    @Test(dataProvider = "navigationTestData", dataProviderClass = TestData.class)
+    public void testNavigationElementNavigatesToCorrespondingPage(String locator, int index, String correspondingPageEndpoint) {
+        if (getIsOnHomePage()) {
+            if (index == -1) {
+                getPage().locator(locator).click();
+            } else {
+                assertThat(getPage().locator(locator)).hasCount(3);
+
+                getPage().locator(locator).nth(index).click();
+            }
+
+            assertThat(getPage()).hasURL(correspondingPageEndpoint);
+        } else {
+            Assert.fail();
+        }
     }
 
     @Test
@@ -32,37 +129,35 @@ public final class NavigationTest extends BaseTest {
         } else {
             Assert.fail();
         }
-
     }
 
     @Test
     public void testMenNavigatesToMenPage() { //структура: меню->страница
-    getPage().navigate(BASE_URL);
+        getPage().navigate(BASE_URL);
 //    page.getByText(MEN).click(); // показать все локаторые, которые он видит
-    getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(TestData.MEN).setExact(true)).click();
+        getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(TestData.MEN).setExact(true)).click();
 
-    assertThat(getPage()).hasURL(BASE_URL + MEN_END_POINT);
+        assertThat(getPage()).hasURL(BASE_URL + MEN_END_POINT);
     }
 
-   @Test
+    @Test
     public void testAccessoriesNavigatesToAccessoriesPage() {
         getPage().navigate(BASE_URL);
 //        getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(ACCESSORIES).setExact(true)).click();
-       getPage().getByRole(AriaRole.NAVIGATION).getByRole(AriaRole.LINK, new Locator.GetByRoleOptions().setName(TestData.ACCESSORIES)).click();
+        getPage().getByRole(AriaRole.NAVIGATION).getByRole(AriaRole.LINK, new Locator.GetByRoleOptions().setName(TestData.ACCESSORIES)).click();
 
 //        getPage().locator("#navbar").getByRole(AriaRole.LINK, new Locator.GetByRoleOptions().setName("accessories")).click();
 
         assertThat(getPage()).hasURL(BASE_URL + ACCESSORIES_END_POINT);
-   }
+    }
 
-   @Test
+    @Test
     public void testAboutNavigatesToAboutPage() {
         getPage().navigate(BASE_URL);
         getPage().getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName(ABOUT).setExact(true)).click();
 
         assertThat(getPage()).hasURL(BASE_URL + ABOUT_END_POINT);
-
-   }
+    }
 
 
 }
